888624f868ac29ed989fe3a8502bb570f2071885
soeirotharine-coder <soeirotharine@gmail.com>
2025-10-16 15:33:59 +0000

    posicionar os navios na horizontal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -59,15 +59,22 @@ int main() {
         printf("\n");
     }
 
-    return 0;
-}
-
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
+    int tabuleiroAventureiro[10][10] = {0};
 
+    // --- Navio 1: horizontal ---
+    for (int i = 0; i < 3; i++) {
+        tabuleiroAventureiro[1][i + 2] = 3;
+    }
+
+    // --- Navio 2: vertical ---
+    for (int i = 0; i < 3; i++) {
+        tabuleiroAventureiro[i + 4][7] = 3;
+    }
 
 
 
